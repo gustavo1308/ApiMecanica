6c879397b9f248bdd801a9f4c9443f6ac743d38e
gustavo arjona <gustavoarjona3@gmail.com>
2022-08-06 00:05:50 -0300

    Adicionando pasta dominio com DBset

---
+++ AM.Domain/Carros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AM.Domain
+{
+    public class Carros
+    {
+        [Key]
+        public Guid Placa { get; set; }
+
+        public string Fabricante { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int AnoDeProducao { get; set; }
+
+        public string DefeitoAConsertar { get; set; }
+    }
+}
---
+++ AMBancoDados/ContextoPrincipal.cs
@@ -0,0 +1,17 @@
+﻿using AM.Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+
+namespace AMBancoDados
+{
+    public class ContextoPrincipal : DbContext
+    {
+        public ContextoPrincipal(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Carros> Carros { get; set; }
+    }
+}
